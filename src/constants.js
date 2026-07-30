94014dce762ef8cbb5438287614d379a8e2df803
sumit pal <debuggersumit@gmail.com>
2026-07-30 21:05:34 +0530

    keep data in constant file and add apiError class to the code base

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,23 @@
 const PORT = process.env.PORT || 8000;
 const CORS_ORIGIN = process.env.CORS_ORIGIN?.split(",") || "*";
-export { PORT, CORS_ORIGIN };
+const USER_ROLES_ENUM = {
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project_admin",
+    MEMBER: "member",
+};
+const AVAILABLE_USER_ROLES = Object.values(USER_ROLES_ENUM);
+const Task_Status_Enum = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done",
+};
+const AVAILABLE_TASK_STATUSES = Object.values(Task_Status_Enum);
+
+export {
+    PORT,
+    CORS_ORIGIN,
+    USER_ROLES_ENUM,
+    AVAILABLE_USER_ROLES,
+    Task_Status_Enum,
+    AVAILABLE_TASK_STATUSES,
+};
